4691ddf95e9798940bef38455c1627091e51acbd
M. Cardoso <cardosaum@pm.me>
2021-06-29 18:34:32 -0300

    update templates

--- .templates/main.cpp
+++ .templates/main.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 
 #define all(v) v.begin(), v.end()
-#define SPEED ios_base::sync_with_stdio(false);cin.tie(nullptr)
-#define rep(i,n) for(int i=0; i<(int)(n); i++)
+#define SPEED                                                                  \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(nullptr)
+#define endl '\n'
+#define TESTS                                                                  \
+    ull T;                                                                     \
+    cin >> T;                                                                  \
+    while (T--)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define fi first
 #define se second
 #define mp make_pair
@@ -15,24 +24,28 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long ul;
 typedef unsigned long long ull;
-typedef pair<int,int> ii;
+typedef pair<int, int> pii;
+typedef pair<ull, ull> pullull;
 typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef vector<ul> vul;
 typedef vector<ull> vull;
 typedef vector<string> vs;
-typedef vector<ii> vii;
+typedef vector<pii> vpii;
 typedef vector<long long int> vll;
 typedef vector<char> vc;
 typedef long double ld;
 typedef istringstream iss;
 typedef set<int> si;
-typedef map<int,int> mii;
-typedef map<string,int> msi;
-typedef map<char,int> mci;
+typedef set<ull> sull;
+typedef map<int, int> mii;
+typedef map<ull, ull> mullull;
+typedef map<string, int> msi;
+typedef map<string, string> mss;
+typedef map<char, int> mci;
 
 typedef set<int>::iterator si_it;
-typedef map<int,int>::iterator mii_it;
+typedef map<int, int>::iterator mii_it;
 typedef vector<int>::iterator vit;
 typedef vector<long long int>::iterator vllit;
 
@@ -40,7 +53,4 @@ typedef vector<long long int>::iterator vllit;
 [[maybe_unused]] const int MOD = 1e9 + 7;
 [[maybe_unused]] const int MAXN = 1e6 + 3;
 
-int main() {
-    SPEED;
-
-}
+int main() { SPEED; }
--- .templates/template.sh
+++ .templates/template.sh
@@ -6,8 +6,10 @@ TEMPLATE_DIR=".templates"
 TEMPLATE_DIR=$(echo "$(git rev-parse --show-toplevel)/${TEMPLATE_DIR}")
 MAIN_CPP=$(fd -e.cpp -tf -d1 -a "main.cpp" "${TEMPLATE_DIR}")
 MAKEFILE_SRC=$(fd -tf -d1 -a -i "makefile" "${TEMPLATE_DIR}")
+CLANG_FORMAT_SRC=$(fd --no-ignore --hidden -tf -d1 -a -i ".clang-format" "${TEMPLATE_DIR}")
 MAIN_DEST="main.cpp"
 MAKEFILE_DEST="Makefile"
+CLANG_FORMAT_DEST=".clang-format"
 URL=$(xclip -sel c -o )
 
 echo "${URL}" | rg '(contest/\d+/)?problems?(/\d+)?/[\w\d]+/?$' -o | xargs -d $'\n' sh -c 'for arg do mkdir -p "$arg"; echo -n "$arg" | xclip -sel c; done' _
@@ -17,6 +19,7 @@ curl "${URL}" | pup '.sample-tests' | codeforces_parse_input.py
 
 [[ ! -f "${MAIN_DEST}" ]] && cp "${MAIN_CPP}" "${MAIN_DEST}" || echo "Avoid creation of file"
 [[ ! -f "${MAKEFILE_DEST}" ]] && ln -rs "${MAKEFILE_SRC}" "${MAKEFILE_DEST}" || echo "Avoid creation of file"
+[[ ! -f "${CLANG_FORMAT_DEST}" ]] && ln -rs "${CLANG_FORMAT_SRC}" "${CLANG_FORMAT_DEST}" || echo "Avoid creation of file"
 
 emacsclient -a "" -c -n "${MAIN_DEST}"
 echo -n "${NEW_PWD}" | xclip -sel c
