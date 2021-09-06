0e771bafc9f2eebe0f3f7532b9e998a8816e2f02
Cardosaum <cardosaum@pm.me>
2021-09-06 17:18:33 -0300

    start to write solution for D

---
+++ codeforces/contest/343610/problem/D/main.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(v) v.begin(), v.end()
+#define SPEED                                                                  \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(nullptr)
+#define endl '\n'
+#define TESTS                                                                  \
+    ull T;                                                                     \
+    cin >> T;                                                                  \
+    while (T--)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rint(i, n) for (int i = 0; i < (int)(n); i++)
+#define rull(i, n) for (ull i = 0; i < (ull)(n); i++)
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define ep emplace
+#define te try_emplace
+
+typedef long long ll;
+typedef unsigned long ul;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ull, ull> pullull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ul> vul;
+typedef vector<ull> vull;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<long long int> vll;
+typedef vector<char> vc;
+typedef long double ld;
+typedef istringstream iss;
+typedef set<int> si;
+typedef set<ull> sull;
+typedef map<int, int> mii;
+typedef map<ull, ull> mullull;
+typedef map<string, int> msi;
+typedef map<string, string> mss;
+typedef map<char, int> mci;
+
+typedef set<int>::iterator si_it;
+typedef map<int, int>::iterator mii_it;
+typedef vector<int>::iterator vit;
+typedef vector<long long int>::iterator vllit;
+
+[[maybe_unused]] const int INF = 1e9 + 7;
+[[maybe_unused]] const int MOD = 1e9 + 7;
+[[maybe_unused]] const int MAXN = 1e6 + 3;
+
+void sieve(ull lim, vull &divisors, sull &primes) {
+    divisors[1] = 1;
+    for (ull i = 2; i <= lim; i++) {
+        if (divisors[i] == 0)
+            divisors[i] = i, primes.insert(i);
+        for (ull j : primes) {
+            if (j > divisors[i] or i * j > lim)
+                break;
+            divisors[i * j] = j;
+        }
+    }
+}
+
+int main() {
+    SPEED;
+    vull divisors(1e3 + 3);
+    sull primes;
+    sieve(1e3, divisors, primes);
+    ull n;
+    cin >> n;
+    ull almost_primes = 0;
+    for (ull i = 6; i <= n; i++) {
+        ull prime_divisors = 0;
+        for (auto const &j : primes) {
+            if (i % j == 0)
+                prime_divisors++;
+            if (prime_divisors > 2)
+                break;
+        }
+        if (prime_divisors == 2)
+            almost_primes++;
+    }
+    cout << almost_primes << endl;
+}
