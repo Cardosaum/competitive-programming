616c6ac09ec02abdf50c51728ac71968cd06b281
Cardosaum <cardosaum@pm.me>
2021-11-18 17:35:35 -0300

    add a bunch of problems (not all solved)

---
+++ CodeforcesEducationalCodeforcesRound39RatedforDiv.2/C_StringTransformation.cpp
@@ -0,0 +1,82 @@
+// time-limit: 1000
+// problem-url: https://codeforces.com/contest/946/problem/C
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(v) v.begin(), v.end()
+#define SPEED                                                                  \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(nullptr)
+#define endl '\n'
+#define TESTS                                                                  \
+  ull T;                                                                       \
+  cin >> T;                                                                    \
+  while (T--)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rint(i, n) for (int i = 0; i < (int)(n); i++)
+#define rull(i, n) for (ull i = 0; i < (ull)(n); i++)
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define ep emplace
+#define te try_emplace
+
+typedef long long ll;
+typedef unsigned long ul;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ull, ull> pullull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ul> vul;
+typedef vector<ull> vull;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<long long int> vll;
+typedef vector<char> vc;
+typedef long double ld;
+typedef istringstream iss;
+typedef set<int> si;
+typedef set<ull> sull;
+typedef map<int, int> mii;
+typedef map<ull, ull> mullull;
+typedef map<string, int> msi;
+typedef map<string, string> mss;
+typedef map<char, int> mci;
+
+typedef set<int>::iterator si_it;
+typedef map<int, int>::iterator mii_it;
+typedef vector<int>::iterator vit;
+typedef vector<long long int>::iterator vllit;
+
+[[maybe_unused]] const int INF = 1e9 + 7;
+[[maybe_unused]] const int MOD = 1e9 + 7;
+[[maybe_unused]] const int MAXN = 1e6 + 3;
+
+int main() {
+  SPEED;
+  string s;
+  cin >> s;
+  ull letter = 0;
+  string f = "";
+  rull(i, s.size()) {
+    // cout << "l: " << letter << " s[i]: " << s[i] << " f: " << f.size() <<
+    // endl;
+    if (('a' + letter) >= s[i] && letter < 26)
+      f.push_back((char)('a' + letter++));
+    else
+      f.push_back(s[i]);
+  };
+  // cout << s.size() << endl;
+  // cout << s << endl;
+  if (letter != 26) {
+    cout << -1 << endl;
+    return 0;
+  }
+  cout << f << endl;
+}
